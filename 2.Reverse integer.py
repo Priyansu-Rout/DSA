b2953046850b26c817be3bc935ca8431ef1e4b9f
Priyansu-Rout <priyansurout369@gmail.com>
2025-09-29 16:57:52 +0000

    reverse integer

---
+++ 2.Reverse integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        INT_MAX = 2**31 - 1  # 2147483647
+        INT_MIN = -2**31     # -2147483648
+
+        rev = 0
+        sign = 1 if x >= 0 else -1
+        x = abs(x)
+
+        while x != 0:
+            pop = x % 10
+            x //= 10
+
+            # Check overflow before multiplying by 10
+            if rev > (INT_MAX - pop) // 10:
+                return 0
+
+            rev = rev * 10 + pop
+
+        return sign * rev
